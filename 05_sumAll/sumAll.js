9f3b3451032707aded1bb6098b70d17a04b1de9c
jeff-livers <jefflivers42@gmail.com>
2024-02-09 22:26:37 -0800

    Finish lesson #5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
-
-};
+const sumAll = function(min,max) {
+    let counter = 0;
+    if (!Number.isInteger(min) || min < 0) {
+        return 'ERROR';
+      }
+    if (!Number.isInteger(max) || max < 0) {
+        return 'ERROR';
+    }
+    if(min < max){
+        for(let i = min; i <= max; i++){
+            counter+=i;
+        }
+    } else {
+        for(let i = max; i <= min; i++){
+            counter+=i;
+        }
+    }
+    return counter
+}
+console.log(sumAll(-5,4))
 
 // Do not edit below this line
 module.exports = sumAll;
